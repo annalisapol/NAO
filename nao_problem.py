095acc38f377404734289dce1814059db8fe08d0
Annalisa Poluzzi <annapol2003@gmail.com>
2025-11-16 15:41:04 +0100

    initial and last states constraints

--- nao_problem.py
+++ nao_problem.py
@@ -10,7 +10,7 @@ from moves_helpers import (
 from utils import distance
 
 class NAOProblem(Problem):
-    def __init__(self, state, move_durations, incompatibilities, max_time=MAX_TIME):
+    def __init__(self, max_time=MAX_TIME, initial_state=None, goal=None):
         '''
         Initialize the planning problem with:
         - Initial state
@@ -26,18 +26,20 @@ class NAOProblem(Problem):
             "time": 0,
             #copying the mandatory moves set to avoid modifying the global structure
             "mandatory_left": self.MANDATORY.copy(),
-            "intermediate_count":0
-            "posture": "None"
+            "intermediate_count":0,
+            "posture": "None",
             # changing this to None because "Stand" still needs to be computed, since time is at 0
             # initial posture is not defined yet 
             # "Stand" should be passed to the problem in some way and then compute it's time
+            
+            # keeping track of moves done for forcing initial and final postures
+            "moves_done": []
         }
         
         super().__init__(self.state,None)
-        self.max_time = max_time
-        self.move_durations = move_durations #should be a dict mapping each move to its duration in seconds, maybe at the fastest speed
-        self.incompatibilities = incompatibilities #to be defined, should be a dict mapping each move to the list of incompatible moves
 
+        self.max_time = max_time
+        
     # changing name into actions otherwise it won't override the Problem "actions" class method
     def actions(self, state):
         '''
@@ -47,11 +49,14 @@ class NAOProblem(Problem):
         - Incompatibilities with previous move
         - Posture requirements (standing/sitting)
         '''
+        # must start with StandInit 
+        if state["moves_done"] == []:
+            return ["StandInit"]
+
         #changing name to next_moves to avoid conflict with reserved python keyword "next"
         next_moves = []
 
-        # no logic changes, just adapting to new MOVES dict structure
-
+        # adapting to new MOVES dict structure
         for move, info in MOVES.items():
 
             duration = info["duration"]
@@ -77,7 +82,7 @@ class NAOProblem(Problem):
         result_state["time"]+=info["duration"]
 
         #changing to always access self variables, so we don't change a global structure
-        if action in self.MANDATORY:
+        if action in result_state["mandatory_left"]:
             # importart fix, made a copy so we don't mutate the previous's state "mandatory_left" list
             # every state should have its own copy of the list for independence
             # so the algorithm can explore different branches correctly
@@ -87,6 +92,7 @@ class NAOProblem(Problem):
             result_state["intermediate_count"]+=1
 
         result_state["posture"] = info["produces"]
+        result_state["moves_done"] = state["moves_done"] + [action]
 
         return result_state
 
@@ -101,5 +107,8 @@ class NAOProblem(Problem):
             #accessing self variable again for consistency and modularity
             state["time"] <= self.max_time and
             not state["mandatory_left"] and
-            state["intermediate_count"] >= 5
+            state["intermediate_count"] >= 5 and 
+            state["moves_done"] and
+            # final posture forced to Crouch
+            state["moves_done"][-1] == "Crouch"
         )
